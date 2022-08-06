3bf60550b30b02916f9d8675e9fe2741fc330860
Colin Holzman-Klima <colinhk@icloud.com>
2022-08-06 15:14:57 -0700

    small demonstration of bigint

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("hello\n");
+#include "../include/bigint.h"
+
+int main(int argc, char const *argv[]) {
+    BigInt* bigint = BigInt_FromString("12863437456928734652983746517826345178263417263451872634517623");
+    for (size_t i = bigint->nDigits - 1; i != (size_t) -1; i--) {
+        printf("%hhu", bigint->digits[i]);
+    }
+    printf("\n");
     return 0;
 }
