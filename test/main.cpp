ae42d60aa4187e833ea780291c6b72290d899f61
Colin Holzman-Klima <colinhk@icloud.com>
2022-08-07 16:02:37 -0700

    added tests for resizing; passes all currently; more needed for add

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,16 @@
 extern "C" {
-    // #include "../include/crypt.h"
     #include "../include/bigint.h"
 }
 
 #include "qtest.hpp"
+
+// helper function to make assersions about the contents
+// of digit_t arrays
+void compareArrays(digit_t expected[], digit_t actual[], size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        QTEST_EXPECT(expected[i] == actual[i]);
+    }
+}
 
 QTEST_CASE(BigInt, Init) {
     BigInt* bigInt = BigInt_Init(42);
@@ -22,8 +29,9 @@ QTEST_CASE(BigInt, FromString) {
     QTEST_EXPECT(bigIntPos->nDigits == 2);
     QTEST_EXPECT(bigIntPos->capacity == 2);
     QTEST_EXPECT(bigIntPos->digits != NULL);
-    QTEST_EXPECT(bigIntPos->digits[0] == 2);
-    QTEST_EXPECT(bigIntPos->digits[1] == 4);
+    // QTEST_EXPECT(bigIntPos->digits[0] == 2);
+    // QTEST_EXPECT(bigIntPos->digits[1] == 4);
+    compareArrays((digit_t[]) {2, 4}, bigIntPos->digits, 2);
     BigInt_Free(bigIntPos);
 
     BigInt* bigIntNeg = BigInt_FromString("-42");
@@ -32,8 +40,8 @@ QTEST_CASE(BigInt, FromString) {
     QTEST_EXPECT(bigIntNeg->nDigits == 2);
     QTEST_EXPECT(bigIntNeg->capacity == 2);
     QTEST_EXPECT(bigIntNeg->digits != NULL);
-    QTEST_EXPECT(bigIntPos->digits[0] == 2);
-    QTEST_EXPECT(bigIntPos->digits[1] == 4);
+    QTEST_EXPECT(bigIntNeg->digits[0] == 2);
+    QTEST_EXPECT(bigIntNeg->digits[1] == 4);
     BigInt_Free(bigIntNeg);
 }
 
@@ -68,12 +76,99 @@ QTEST_CASE(BigInt, FromStringLong) {
     BigInt_Free(bigIntLong);
 }
 
-QTEST_CASE(BigInt, Add) {
-    BigInt* bigInt42  = BigInt_Init(42);
-    BigInt* bigInt8   = BigInt_Init(8);
+QTEST_CASE(BigInt, FromStringError) {
+    QTEST_EXPECT(BigInt_FromString("not a number") == NULL);
+    QTEST_EXPECT(BigInt_FromString("-not a number") == NULL);
+    QTEST_EXPECT(BigInt_FromString("1abc") == NULL);
+    QTEST_EXPECT(BigInt_FromString("-1abc") == NULL);
+    QTEST_EXPECT(BigInt_FromString("a") == NULL);
+    QTEST_EXPECT(BigInt_FromString("-a") == NULL);
+    QTEST_EXPECT(BigInt_FromString("23984765283476a23894569") == NULL);
+    QTEST_EXPECT(BigInt_FromString("-23984765283476a23894569") == NULL);
+    QTEST_EXPECT(BigInt_FromString("2398476528347623894569a") == NULL);
+    QTEST_EXPECT(BigInt_FromString("-2398476528347623894569a") == NULL);
+}
+
+QTEST_CASE(BigInt, ToString) {
+    BigInt* bigInt = BigInt_FromString("4266");
+    char* bigIntStr = BigInt_ToString(bigInt);
+    QTEST_EXPECT(bigIntStr != NULL);
+    QTEST_EXPECT(strcmp(bigIntStr, "4266") == 0);
+    free(bigIntStr);
+    BigInt_Free(bigInt);
+}
+
+QTEST_CASE(BigInt, ResizeBigger) {
+    BigInt* bigInt = BigInt_FromString("456");
+    QTEST_EXPECT(BigInt_Resize(bigInt, 10) == BIG_INT_E_SUCCESS);
+    QTEST_EXPECT(bigInt != NULL);
+    QTEST_EXPECT(bigInt->isNegative == false);
+    QTEST_EXPECT(bigInt->nDigits == 3);
+    QTEST_EXPECT(bigInt->capacity == 10);
+    QTEST_EXPECT(bigInt->digits != NULL);
+    QTEST_EXPECT(bigInt->digits[0] == 6);
+    QTEST_EXPECT(bigInt->digits[1] == 5);
+    QTEST_EXPECT(bigInt->digits[2] == 4);
+    BigInt_Free(bigInt);
+}
+
+QTEST_CASE(BigInt, ResizeSmaller) {
+    BigInt* bigInt = BigInt_FromString("456");
+    QTEST_EXPECT(BigInt_Resize(bigInt, 1) == BIG_INT_E_SUCCESS);
+    QTEST_EXPECT(bigInt != NULL);
+    QTEST_EXPECT(bigInt->isNegative == false);
+    QTEST_EXPECT(bigInt->nDigits == 1);
+    QTEST_EXPECT(bigInt->capacity == 1);
+    QTEST_EXPECT(bigInt->digits != NULL);
+    QTEST_EXPECT(bigInt->digits[0] == 6);
+    BigInt_Free(bigInt);
+}
+
+QTEST_CASE(BigInt, ResizeSame) {
+    BigInt* bigInt = BigInt_FromString("456");
+    QTEST_EXPECT(BigInt_Resize(bigInt, 3) == BIG_INT_E_SUCCESS);
+    QTEST_EXPECT(bigInt != NULL);
+    QTEST_EXPECT(bigInt->isNegative == false);
+    QTEST_EXPECT(bigInt->nDigits == 3);
+    QTEST_EXPECT(bigInt->capacity == 3);
+    QTEST_EXPECT(bigInt->digits != NULL);
+    QTEST_EXPECT(bigInt->digits[0] == 6);
+    QTEST_EXPECT(bigInt->digits[1] == 5);
+    QTEST_EXPECT(bigInt->digits[2] == 4);
+    BigInt_Free(bigInt);
+}
+
+QTEST_CASE(BigInt, AddSimple) {
+    BigInt* bigInt42  = BigInt_FromString("42");
+    BigInt* bigInt8   = BigInt_FromString("8");
     BigInt* bigIntSum = BigInt_Init(0);
 
-    QTEST_EXPECT(BigInt_Add(bigInt42, bigInt8, bigIntSum) == E_SUCCESS);
+    QTEST_EXPECT(BigInt_Add(bigInt42, bigInt8, bigIntSum) == BIG_INT_E_SUCCESS);
+
+    // check the summands dont change
+    QTEST_EXPECT(bigInt42 != NULL);
+    QTEST_EXPECT(bigInt42->capacity == 2);
+    QTEST_EXPECT(bigInt42->nDigits == 2);
+    QTEST_EXPECT(bigInt42->isNegative == false);
+    QTEST_EXPECT(bigInt42->digits != NULL);
+    QTEST_EXPECT(bigInt42->digits[0] == 2);
+    QTEST_EXPECT(bigInt42->digits[1] == 4);
+
+    QTEST_EXPECT(bigInt8 != NULL);
+    QTEST_EXPECT(bigInt8->capacity == 1);
+    QTEST_EXPECT(bigInt8->nDigits == 1);
+    QTEST_EXPECT(bigInt8->isNegative == false);
+    QTEST_EXPECT(bigInt8->digits != NULL);
+    QTEST_EXPECT(bigInt8->digits[0] == 8);
+
+    // check the result is correct
+    QTEST_EXPECT(bigIntSum != NULL);
+    QTEST_EXPECT(bigIntSum->capacity == 2);
+    QTEST_EXPECT(bigIntSum->nDigits == 2);
+    QTEST_EXPECT(bigIntSum->isNegative == false);
+    QTEST_EXPECT(bigIntSum->digits != NULL);
+    QTEST_EXPECT(bigIntSum->digits[0] == 0);
+    QTEST_EXPECT(bigIntSum->digits[1] == 5);
 
     BigInt_Free(bigInt42);
     BigInt_Free(bigInt8);
