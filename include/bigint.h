b5dcdf57344327242ae78311900412f5d65b3e0b
Colin Holzman-Klima <colinhk@icloud.com>
2022-08-07 16:02:05 -0700

    added resizing; add function works with simple positive addition; needs fixing to handle negatives

--- include/bigint.h
+++ include/bigint.h
@@ -38,14 +38,14 @@ typedef struct {
 } BigInt;
 
 typedef enum {
-    E_SUCCESS,
-    E_FAILURE,
-    E_OVERFLOW,
-    E_UNDERFLOW,
-    E_DIVISION_BY_ZERO,  // divizion by zero
-    E_MEMORY_ALLOCATION,  // memory allocation error
-    E_MODULUS,  // Montgomery multiplication given even modulus
-    E_NULL_ARGUMENT  // NULL passed as argument
+    BIG_INT_E_SUCCESS,
+    BIG_INT_E_FAILURE,
+    BIG_INT_E_OVERFLOW,
+    BIG_INT_E_UNDERFLOW,
+    BIG_INT_E_DIVISION_BY_ZERO,  // divizion by zero
+    BIG_INT_E_MEMORY_ALLOCATION,  // memory allocation error
+    BIG_INT_E_MODULUS,  // Montgomery multiplication given even modulus
+    BIG_INT_E_NULL_ARGUMENT  // NULL passed as argument
 } BigInt_Error_t;
 
 typedef struct {
@@ -57,14 +57,14 @@ typedef struct {
 } BigInt_ErrorDesc_t;
 
 BigInt_ErrorDesc_t eDesc[] = {
-    { E_SUCCESS, "operation successful" },
-    { E_FAILURE, "operation failure" },
-    { E_OVERFLOW, "operation resulted in overflow" },
-    { E_UNDERFLOW, "operation resulted in underflow" },
-    { E_DIVISION_BY_ZERO, "operation attempted division by zero" },
-    { E_MEMORY_ALLOCATION, "memory allocation failed" },
-    { E_MODULUS, "Montgomery multiplication was given an even modulus" },
-    { E_NULL_ARGUMENT, "operation was given a NULL pointer argument" }
+    { BIG_INT_E_SUCCESS, "operation successful" },
+    { BIG_INT_E_FAILURE, "operation failure" },
+    { BIG_INT_E_OVERFLOW, "operation resulted in overflow" },
+    { BIG_INT_E_UNDERFLOW, "operation resulted in underflow" },
+    { BIG_INT_E_DIVISION_BY_ZERO, "operation attempted division by zero" },
+    { BIG_INT_E_MEMORY_ALLOCATION, "memory allocation failed" },
+    { BIG_INT_E_MODULUS, "Montgomery multiplication was given an even modulus" },
+    { BIG_INT_E_NULL_ARGUMENT, "operation was given a NULL pointer argument" }
 };
 
 void BigInt_PrintError(BigInt_Error_t code) {
@@ -76,6 +76,9 @@ BigInt* BigInt_FromString(const char* str);
 // BigInt* BigInt_Copy(BigInt* other);
 // int8_t  BigInt_Compare(BigInt* other);
 void BigInt_Free(BigInt* bigInt);
+// helper functions
+char* BigInt_ToString(BigInt *bigInt);
+BigInt_Error_t BigInt_Resize(BigInt *bigInt, size_t newCapacity);
 BigInt_Error_t BigInt_Add(BigInt* left, BigInt* right, BigInt* result);
 
 BigInt* BigInt_Init(size_t capacity) {
@@ -86,8 +89,10 @@ BigInt* BigInt_Init(size_t capacity) {
     ret->capacity = capacity;
     ret->nDigits = 0;
     ret->digits = (digit_t*) malloc(sizeof(digit_t) * capacity);
-    if (!(ret->digits))
+    if (!(ret->digits)) {
+        free(ret);  // first allocation (ret) succeeded, so free it before return
         return NULL;
+    }
     return ret;
 }
 
@@ -105,12 +110,14 @@ BigInt* BigInt_FromString(const char* str) {
 
     ret->nDigits = nDigits;
     ret->isNegative = isNegative;
-    for (size_t i = nDigits - 1; i != (size_t) -1; i--) {
-        char c = str[i];
+    // for (size_t i = nDigits - 1; i != (size_t) -1; i--) {
+    for (size_t i = 0; i < nDigits; i++) {
+        char c = str[nDigits - 1 - i];
         if (isdigit(c)) {
-            ret->digits[nDigits - 1 - i] = (digit_t) (c - '0');
+            ret->digits[i] = (digit_t) (c - '0');
         } else {
             // invalid character for integer type (i.e., NaN)
+            BigInt_Free(ret);  // REMEMBER TO FREE ALLOCATED MEMORY
             return NULL;
         }
     }
@@ -122,29 +129,83 @@ void BigInt_Free(BigInt* bigint) {
     free(bigint);
 }
 
+// NOTE: caller is responsible for free'ing the allocated
+// buffer that is returned
+char* BigInt_ToString(BigInt *bigInt) {
+    // the length of the string is the number of digits, plus 1 for '-'
+    // prefix if it is negative
+    size_t strLen = (bigInt->isNegative) ? bigInt->nDigits + 1 : bigInt->nDigits;
+    char* str = (char*) malloc(strLen * sizeof(char));
+
+    if (!str)
+        return NULL;
+
+    for (size_t i = 0; i < strLen; i++) {
+        str[i] = (char) (bigInt->digits[strLen - 1 - i] + '0');
+    }
+
+    return str;
+}
+
+BigInt_Error_t BigInt_Resize(BigInt *bigInt, size_t newCapacity) {
+    if (newCapacity <= bigInt->capacity) {
+        bigInt->nDigits = (newCapacity > bigInt->nDigits) ? bigInt->nDigits : newCapacity;
+    } else {
+        bigInt->digits = (digit_t*) realloc(bigInt->digits, newCapacity);
+        if (!(bigInt->digits)) {
+            return BIG_INT_E_MEMORY_ALLOCATION;
+        }
+    }
+    bigInt->capacity = newCapacity;
+    return BIG_INT_E_SUCCESS;
+}
+
 BigInt_Error_t BigInt_Add(BigInt* left, BigInt* right, BigInt* result) {
     if (left == NULL || right == NULL || result == NULL)
-        return E_NULL_ARGUMENT;
+        return BIG_INT_E_NULL_ARGUMENT;
 
-    // init ptrs so a points to longer int or a, and b
+    // 1. init ptrs so a points to longer int or a, and b
     // points to shorter int or b
-    // BigInt *a, *b;
-    // if (left->nDigits > right->nDigits) {
-    //     a = left;
-    //     b = right;
-    // } else {
-    //     a = right;
-    //     b = left;
-    // }
+    BigInt *a, *b;
+    if (left->nDigits > right->nDigits) {
+        a = left;
+        b = right;
+    } else {
+        a = right;
+        b = left;
+    }
 
-    // digit_t *aMsdPtr = BI_MSDPTR(a);
-    // digit_t *bMsdPtr = BI_MSDPTR(b);
-    // digit_t *aPtr = BI_LSDPTR(a);
-    // digit_t *bPtr = BI_LSDPTR(b);
-    // digit_t *resPtr = BI_LSDPTR(result);
-    // BigInt_Error_t eCode = E_SUCCESS;
+    // 1.5. set the sign of the result
 
-    return E_FAILURE;
+    // 2. resize the result if necessary
+    BigInt_Resize(result, a->capacity);
+    result->nDigits = a->nDigits;
+
+    size_t i = 0;
+    digit_t carry = 0;
+    // 3. loop over the shorter int from lsd to msd
+    for (; i < b->nDigits; i++) {
+        digit_t t_sum = b->digits[i] + a->digits[i] + carry;
+        carry = t_sum / 10;  // TODO: divide by the base instead of 10 ?
+        result->digits[i] = t_sum % 10;
+    }
+
+    // 4. loop over the longer int to move those digits to the result
+    // accounting for any carry
+    for (; i < a->nDigits; i++) {
+        digit_t t_sum = a->digits[i] + carry;
+        carry = t_sum / 10;  // TODO: divide by the base instead of 10 ?
+        result->digits[i] = t_sum % 10;  // same as above here ?
+    }
+
+    // 5. check if we need to append a final carry digit to the end of the result int
+    if (carry) {
+        BigInt_Resize(result, result->capacity + 1);
+        result->digits[result->capacity - 1] = carry;
+        (result->nDigits)++;
+    }
+
+    return BIG_INT_E_SUCCESS;
 }
 
 #endif  // BIGINT_H_
