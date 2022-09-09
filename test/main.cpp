9c2adfd6a960f0c1c0fbcda3cd414356be983b9b
Colin Holzman-Klima <colinhk@icloud.com>
2022-09-09 10:51:38 -0700

    includes tests for aes.h

--- test/main.cpp
+++ test/main.cpp
@@ -1,14 +1,19 @@
 extern "C" {
-    #include "../include/bigint.h"
+    #include "../include/aes.h"
+    // #include "../include/bigint.h"
 }
 
 #include "qtest.hpp"
 
-#include "test_utils.hpp"
-#include "test_io_internals.hpp"
-#include "test_add2.hpp"
-#include "test_subtract2.hpp"
-#include "test_multiply.hpp"
+// bigint tests:
+// #include "test_utils.hpp"
+// #include "test_io_internals.hpp"
+// #include "test_add2.hpp"
+// #include "test_subtract2.hpp"
+// #include "test_multiply.hpp"
+
+// aes tests:
+#include "test_aes.hpp"
 
 int main(int argc, char const *argv[]) {
     QTEST_RUN_ALL();
