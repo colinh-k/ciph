30bd9eb4963686c040defbdc52865f368b952d81
Colin Holzman-Klima <colinhk@icloud.com>
2022-09-09 10:50:39 -0700

    contains global typedefs

---
+++ include/common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+// a header that contains top-level typdefs
+// and forward declarations needed by several
+// modules
+
+#include <stdint.h>
+
+typedef uint8_t byte;
+
+#endif  // COMMON_H_
